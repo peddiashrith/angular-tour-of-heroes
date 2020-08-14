9890c6f27ccc8df4121b84adc1a89a02e318b324
peddiashrith <ashrithkumarpeddi@gmail.com>
2020-08-14 20:30:20 +0530

    modified 3 files

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,8 +7,12 @@ import { Hero } from '../hero';
     styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-    hero: Hero = { id: 1, name: 'saketh' };
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm',
+    };
+
     constructor() {}
 
-    ngOnInit(): void {}
+    ngOnInit() {}
 }
